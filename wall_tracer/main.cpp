48e74dd95f1eddd19d63396e26e26a1df7f9fe6c
joonahn <sdeveloper@naver.com>
2016-07-24 23:59:37 +0900

    Various Method added

--- wall_tracer/main.cpp
+++ wall_tracer/main.cpp
@@ -42,7 +42,9 @@ int main()
 	int IRF = 0, IRB = 0, IRH = 0; 
 
 	// Threshold Variables
-	int th1 = 500, th2 = 450, th3 = 300;
+	int th1 = 500, th2 = 400, med = 350, th3 = 300, th4=200;
+	int delta = 0;
+	int heading = 0;
 
 	// Debug string
 	char str[200];
@@ -70,23 +72,37 @@ int main()
 		
 
 		//Read Sensor Value
-		IRB = adc_read(5);
+		IRB = adc_read(1);
 		IRF = adc_read(3);
-		IRH = adc_read(1);
+		IRH = adc_read(5);
+
+
+
+		//ALIGN
+		heading = IRF-IRB;
 
 		// Print Debug string
 		sprintf(str, "IRF : %d\n IRB : %d\nIRH : %d\n\n", IRF, IRB, IRH);
 		uart_puts(str);
 
-		_delay_ms(100);
-
-		//Hello World
-
-		if(IRH>th2)
+		//Make a big turn
+		/*
+		if(IRH>th4)
 		{
 			turn_right();
+			uart_puts("IRH>th4\n");
 			continue;
 		}
+		else */if(IRF<th4)
+		{
+			// turn_left();
+			pwm(1,10);
+			pwm(2,7.5);
+			uart_puts("out of th4\n");
+			continue;
+		}
+
+		/****** THRESHOLD method
 		else if(IRF>th1)
 		{
 			//(LEFT, RIGHT) = (1, 0.75)
@@ -94,22 +110,82 @@ int main()
 			pwm(2, 7.5);
 			continue;
 		}
-		else if(IRF<th3)
-		{
-			turn_left();
-			continue;
-		}
+		
 		else if(IRF<th2)
 		{
 			//(LEFT, RIGHT) = (0.75, 1)
 			pwm(1, 7.5);
 			pwm(2, 10);
 			continue;
+		}*/
+
+		/****** DELTA method
+		//AVG-IRF
+		delta = (350)-IRF;
+		if(delta>0)
+		{
+			pwm(1,10-(delta>>7));
+			pwm(2,10);
 		}
-		
+		else if(delta <0)
+		{
+			pwm(1,10);
+			pwm(2,10+(delta>>7));
+		} */
 
-		pwm(1, 10);
-		pwm(2, 10);
+		/***** heading method
+		if(heading>0)
+		{
+			pwm(1,10);
+			pwm(2,10-(heading>>5));
+			uart_puts("positive heading\n");
+		}
+		if(heading<0)
+		{
+			pwm(1,10-(heading>>5));
+			pwm(2,10);
+			uart_puts("negative heading\n");
+		}
+		*/
 
+		if(IRF<th3)
+		{
+			delta = th3-IRF;
+			pwm(1,10-delta/20.0);
+			pwm(2,10);
+			uart_puts("in th3-th4 area\n");
+			continue;
+		}
+		else if(IRF<th2)
+		{
+			if(heading>0)
+			{
+				pwm(1,10);
+				pwm(2,10-heading/20.0);
+				uart_puts("positive heading\n");
+			}
+			if(heading<0)
+			{
+				pwm(1,10+heading/20.0);
+				pwm(2,10);
+				uart_puts("negative heading\n");
+			}
+			continue;
+		}
+		else if(IRF<th1)
+		{
+			delta = IRF-th2;
+			pwm(1,10);
+			pwm(2,10-delta/20.0);
+			uart_puts("in th1-th2 area\n");
+			continue;
+		}
+		else
+		{
+			pwm(1,10);
+			pwm(2,7.5);
+			uart_puts("out of th1\n");
+			continue;
+		}
 	}
 }
--- wall_tracer/sensor_test.cpp
+++ wall_tracer/sensor_test.cpp
@@ -12,16 +12,21 @@
 #include <stdio.h>
 #include "wall_tracer.h"
 
- int __s_main()
+ int __AS_main()
  {
 	// Sensor variables,
 	// IRF: front, IRB: left-front, IRH: HEAD
 	int IRF = 0, IRB = 0, IRH = 0;
 
+	// Threshold Variables
+	int th1 = 500, th2 = 400, med = 350, th3 = 300, th4=200;
+	int delta = 0;
+	int heading = 0;
+
 	// Debug string
 	char str[200];
 
-	//Init functions
+	//Initialize functions
 	uart_init();
 
 	while(1)
@@ -31,9 +36,61 @@
 		//IRB = adc_read(2);
 		//IRH = adc_read(0);
 
-		IRB = adc_read(5);
+		IRB = adc_read(1);
 		IRF = adc_read(3);
-		IRH = adc_read(1);
+		IRH = adc_read(5);
+		//ALIGN
+		heading = IRF-IRB;
+
+		// Print Debug string
+		sprintf(str, "IRF : %d\n IRB : %d\nIRH : %d\n\n", IRF, IRB, IRH);
+		uart_puts(str);
+
+		//Make a big turn
+		if(IRF<th4)
+		{
+			// turn_left();
+			pwm(1,10);
+			pwm(2,7.5);
+			uart_puts("out of th4\n\t(10, 7.5)\n");
+			continue;
+		}
+
+		if(IRF<th3)
+		{
+			delta = th3-IRF;
+			sprintf(str, "in th3-th4 area\n\t(%f, %f)\n", 10-delta/20.0, 10.0);
+			uart_puts(str);
+			continue;
+		}
+		else if(IRF<th2)
+		{
+			if(heading>0)
+			{
+				sprintf(str, "positive heading\n\t(%f, %f)\n", 10.0, 10-heading/20.0);
+				uart_puts(str);
+			}
+			if(heading<0)
+			{
+				sprintf(str, "negative heading\n\t(%f, %f)\n", 10-heading/20.0, 10.0);
+				uart_puts(str);
+			}
+			continue;
+		}
+		else if(IRF<th1)
+		{
+			delta = IRF-th2;
+			sprintf(str, "in th1-th2 area\n\t(%f, %f)\n", 10.0, 10-delta/20.0);
+			uart_puts(str);
+			continue;
+		}
+		else
+		{
+			pwm(1,10);
+			pwm(2,7.5);
+			uart_puts("out of th1\n");
+			continue;
+		}
 
 		// Print Debug string
 		sprintf(str, "IRF : %d\n IRB : %d\nIRH : %d\n\n", IRF, IRB, IRH);
